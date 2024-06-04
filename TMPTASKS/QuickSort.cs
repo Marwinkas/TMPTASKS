f8f3ecc7a2efad5c789aa6269c203e66ff8a7c71
Marwinkas <marwinkas@mail.ru>
2024-06-04 20:48:46 +0300

    Задание 5

---
+++ TMPTASKS/QuickSort.cs
@@ -0,0 +1,46 @@
+﻿namespace TMPTASKS
+{
+    public class QuickSort
+    {
+        static void Swap(ref char x, ref char y)
+        {
+            char t = x;
+            x = y;
+            y = t;
+        }
+        static int Partition(char[] array, int minIndex, int maxIndex)
+        {
+            var pivot = minIndex - 1;
+            for (var i = minIndex; i < maxIndex; i++)
+            {
+                if (array[i] < array[maxIndex])
+                {
+                    pivot++;
+                    Swap(ref array[pivot], ref array[i]);
+                }
+            }
+            pivot++;
+            Swap(ref array[pivot], ref array[maxIndex]);
+            return pivot;
+        }
+        static char[] Sort(char[] array, int minIndex, int maxIndex)
+        {
+            if (minIndex >= maxIndex)
+            {
+                return array;
+            }
+
+            int pivotIndex = Partition(array, minIndex, maxIndex);
+            Sort(array, minIndex, pivotIndex - 1);
+            Sort(array, pivotIndex + 1, maxIndex);
+
+            return array;
+        }
+        public static void Output()
+        {
+            char[] array = ReverseString.Get(StringManager._text).ToCharArray();
+            Sort(array, 0, array.Length - 1);
+            Console.WriteLine("Отсортированная строка методом быстрой сортировки: " + new string(array));
+        }
+    }
+}
